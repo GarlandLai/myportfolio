384532f5211e5a138b80d1c8542ef609ffbb59dd
Garland Lai <garlandlai24@gmail.com>
2020-09-29 16:50:34 -0700

    create main to switch between routes to display the correct component

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 class Landing extends Component {
 	render() {
 		return (
 			<div>
-				<h1>Landing</h1>
+				<h1>Landing Page</h1>
 			</div>
 		);
 	}
