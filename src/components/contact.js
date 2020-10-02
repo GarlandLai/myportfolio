fb461a9247c6d86f1b30c75d9167172bfd9d0737
Garland Lai <garlandlai24@gmail.com>
2020-10-01 18:08:21 -0700

    update first half of the contact page with avatart and some placeholder text

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,7 +6,17 @@ class Contact extends Component {
 		return (
 			<div className="contact-body">
 				<Grid className="contact-grid">
-					<Cell col={6}>Column1</Cell>
+					<Cell col={6}>
+						<h2>Garland Lai</h2>
+						<img
+							src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+							alt="avatar"
+							style={{ height: '250px' }}
+						/>
+						<p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>
+							Add some information here
+						</p>
+					</Cell>
 					<Cell col={6}>Column2</Cell>
 				</Grid>
 			</div>
