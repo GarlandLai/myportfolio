b54703c99ef2a21cd6ac23a16746d77f4ceb21cc
Garland Lai <garlandlai24@gmail.com>
2020-10-06 17:46:22 -0700

    turn Skills into functional component

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Grid, Cell } from 'react-mdl';
 
-class Skills extends Component {
-	render() {
-		return (
-			<Grid>
-				<Cell col={12}>
-					<div style={{ display: 'inline', overflow: 'none', width: '80%' }}>
-						<p style={{ fontSize: '14px', fontWeight: '400' }}>
-							Javascript | React | JQuery | Node | Ruby on Rails | Sinatra |
-							PostgreSQL | HTML | CSS | Unit testing |Jasmine | Jest | Mocha |
-							Debugging | Redux | Github | Basic AWS | Firebase | Webpack | APIs
-							| Object Oriented Programming | Heroku | SCRUM/Agile | SaaS |Slack
-							| Airtable | Confluence | Project Management | Customer Service |
-							Microsoft Office
-						</p>
-					</div>
-				</Cell>
-			</Grid>
-		);
-	}
-}
+const Skills = () => {
+	return (
+		<Grid>
+			<Cell col={12}>
+				<div style={{ display: 'inline', width: '80%' }}>
+					<p style={{ fontSize: '14px', fontWeight: '400' }}>
+						Javascript | React | JQuery | Node | Ruby on Rails | Sinatra |
+						PostgreSQL | HTML | CSS | Unit testing |Jasmine | Jest | Mocha |
+						Debugging | Redux | Github | Basic AWS | Firebase | Webpack | APIs |
+						Object Oriented Programming | Heroku | SCRUM/Agile | SaaS |Slack |
+						Airtable | Confluence | Project Management | Customer Service |
+						Microsoft Office
+					</p>
+				</div>
+			</Cell>
+		</Grid>
+	);
+};
 
 export default Skills;
