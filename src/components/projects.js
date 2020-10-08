a6391fca52637f88b85c60d601ec0dc2df5e4d42
Garland Lai <garlandlai24@gmail.com>
2020-10-08 14:34:30 -0700

    add info to react tab projects. Also fix styling

--- src/components/projects.js
+++ src/components/projects.js
@@ -23,61 +23,103 @@ class Projects extends Component {
 		if (this.state.activeTab === 0) {
 			return (
 				<div className="project-grid">
-					<Card shadow={5} style={{ minwidth: '450', margin: 'auto' }}>
+					<Card
+						shadow={5}
+						style={{ minWidth: '450px', height: '450px', margin: 'auto' }}
+					>
 						<CardTitle
 							style={{
 								color: '#fff',
-								height: '176px',
+								height: '225px',
 								background:
 									'url(https://create-react-app.dev/img/logo-og.png) center / cover',
 							}}
 						>
-							React Project #1
+							The Good Burger
 						</CardTitle>
-						<CardText>Add description of project</CardText>
-						<CardActions border>
-							<Button colored>Github</Button>
-							<Button colored>Live Demo</Button>
+						<CardText style={{ height: '130px' }}>
+							<p>
+								Login or create account and create the perfect burger for
+								delivery!
+							</p>
+							<p>Technology Used:</p>
+						</CardText>
+						<CardActions border style={{ padding: '0' }}>
+							<Button colored>
+								<a
+									href="https://github.com/GarlandLai/Burger-Builder"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									GitHub
+								</a>
+							</Button>
+							<Button colored>
+								<a
+									href="https://react-my-burger-a16da.web.app"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									Live Demo
+								</a>
+							</Button>
 							<CardMenu style={{ color: '#fff' }}>
 								<IconButton name="share" />
 							</CardMenu>
 						</CardActions>
 					</Card>
-					<Card shadow={5} style={{ minwidth: '450', margin: 'auto' }}>
+					<Card
+						shadow={5}
+						style={{ minWidth: '450px', height: '450px', margin: 'auto' }}
+					>
 						<CardTitle
 							style={{
 								color: '#fff',
-								height: '176px',
+								height: '225px',
 								background:
 									'url(https://create-react-app.dev/img/logo-og.png) center / cover',
 							}}
 						>
-							React Project #1
+							ZIAI
 						</CardTitle>
-						<CardText>Add description of project</CardText>
-						<CardActions border>
-							<Button colored>Github</Button>
-							<Button colored>Live Demo</Button>
-							<CardMenu style={{ color: '#fff' }}>
-								<IconButton name="share" />
-							</CardMenu>
-						</CardActions>
-					</Card>
-					<Card shadow={5} style={{ minwidth: '450', margin: 'auto' }}>
-						<CardTitle
-							style={{
-								color: '#fff',
-								height: '176px',
-								background:
-									'url(https://create-react-app.dev/img/logo-og.png) center / cover',
-							}}
-						>
-							React Project #1
-						</CardTitle>
-						<CardText>Add description of project</CardText>
-						<CardActions border>
-							<Button colored>Github</Button>
-							<Button colored>Live Demo</Button>
+						<CardText style={{ height: '130px' }}>
+							Meditation journaling application. Listen to the meditation and
+							write some journal entries outlining you experiences!
+							<p>
+								Login or create account and create the perfect burger for
+								delivery!
+							</p>
+							<p>Technology Used:</p>
+						</CardText>
+
+						<CardActions border style={{ padding: '0', alignItems: 'center' }}>
+							<Button colored>
+								<a
+									href="https://github.com/GarlandLai/Capstone-2.0"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									GitHub Front-end
+								</a>
+							</Button>
+							<Button colored>
+								<a
+									href="https://github.com/GarlandLai/capstone-backend"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									GitHub Back-end
+								</a>
+							</Button>
+							<Button colored>
+								<a
+									href="https://react-my-burger-a16da.web.app"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									Live Demo
+								</a>
+							</Button>
 							<CardMenu style={{ color: '#fff' }}>
 								<IconButton name="share" />
 							</CardMenu>
@@ -88,11 +130,14 @@ class Projects extends Component {
 		} else if (this.state.activeTab === 1) {
 			return (
 				// Project 2
-				<Card shadow={5} style={{ minwidth: '450', margin: 'auto' }}>
+				<Card
+					shadow={5}
+					style={{ minWidth: '450px', height: '450px', margin: 'auto' }}
+				>
 					<CardTitle
 						style={{
 							color: '#fff',
-							height: '176px',
+							height: '225px',
 							background:
 								'url(https://create-react-app.dev/img/logo-og.png) center / cover',
 						}}
@@ -112,11 +157,14 @@ class Projects extends Component {
 		} else if (this.state.activeTab === 2) {
 			return (
 				// Project 3
-				<Card shadow={5} style={{ minwidth: '450', margin: 'auto' }}>
+				<Card
+					shadow={5}
+					style={{ minWidth: '450px', height: '450px', margin: 'auto' }}
+				>
 					<CardTitle
 						style={{
 							color: '#fff',
-							height: '176px',
+							height: '225px',
 							background:
 								'url(https://create-react-app.dev/img/logo-og.png) center / cover',
 						}}
