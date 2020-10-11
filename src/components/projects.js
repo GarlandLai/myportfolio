b57a1e4767813108904a348f5fd7a715f3f23ac6
Garland Lai <garlandlai24@gmail.com>
2020-10-11 12:13:43 -0700

    remove tabs from projects and style

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import {
-	Tabs,
-	Tab,
 	Grid,
 	Cell,
 	Card,
@@ -14,237 +12,197 @@ import {
 } from 'react-mdl';
 
 class Projects extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { activeTab: 0 };
-	}
-
-	toggleCategories() {
-		if (this.state.activeTab === 0) {
-			return (
-				<div className="project-grid">
-					<Card
-						shadow={5}
-						style={{
-							minWidth: '440px',
-							height: '450px',
-							margin: 'auto',
-						}}
-					>
-						<CardTitle
-							style={{
-								color: '#fff',
-								height: '225px',
-								background:
-									'url(https://create-react-app.dev/img/logo-og.png) center / cover',
-							}}
-						>
-							The Good Burger
-						</CardTitle>
-						<CardText style={{ height: '130px' }}>
-							<p>
-								Login or create account and create the perfect burger for
-								delivery!
-							</p>
-							<p>Technology Used:</p>
-						</CardText>
-						<CardActions border style={{ padding: '0' }}>
-							<Button colored>
-								<a
-									href="https://github.com/GarlandLai/Burger-Builder"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									GitHub
-								</a>
-							</Button>
-							<Button colored>
-								<a
-									href="https://react-my-burger-a16da.web.app"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									Live Demo
-								</a>
-							</Button>
-							<CardMenu style={{ color: '#fff' }}>
-								<IconButton name="share" />
-							</CardMenu>
-						</CardActions>
-					</Card>
-					<Card
-						shadow={5}
-						style={{
-							minWidth: '440px',
-							height: '450px',
-							margin: 'auto',
-						}}
-					>
-						<CardTitle
-							style={{
-								color: '#fff',
-								height: '225px',
-								background:
-									'url(https://create-react-app.dev/img/logo-og.png) center / cover',
-							}}
-						>
-							ZIAI
-						</CardTitle>
-						<CardText style={{ height: '130px' }}>
-							Meditation journaling application. Listen to the meditation and
-							write some journal entries outlining you experiences!
-							<p>
-								Login or create account and create the perfect burger for
-								delivery!
-							</p>
-							<p>Technology Used:</p>
-						</CardText>
-
-						<CardActions border style={{ padding: '0', alignItems: 'center' }}>
-							<Button colored>
-								<a
-									href="https://github.com/GarlandLai/Capstone-2.0"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									GitHub Front-end
-								</a>
-							</Button>
-							<Button colored>
-								<a
-									href="https://github.com/GarlandLai/capstone-backend"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									GitHub Back-end
-								</a>
-							</Button>
-							<Button colored>
-								<a
-									href="https://react-my-burger-a16da.web.app"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									Live Demo
-								</a>
-							</Button>
-							<CardMenu style={{ color: '#fff' }}>
-								<IconButton name="share" />
-							</CardMenu>
-						</CardActions>
-					</Card>
-				</div>
-			);
-		} else if (this.state.activeTab === 1) {
-			return (
-				// Project 2
-				<div className="project-grid">
-					<Card
-						shadow={5}
-						style={{
-							minWidth: '440px',
-							height: '450px',
-							margin: 'auto',
-						}}
-					>
-						<CardTitle
-							style={{
-								color: '#fff',
-								height: '225px',
-								background:
-									'url(https://create-react-app.dev/img/logo-og.png) center / cover',
-							}}
-						>
-							ZIAI
-						</CardTitle>
-						<CardText style={{ height: '130px' }}>
-							Meditation journaling application. Listen to the meditation and
-							write some journal entries outlining you experiences!
-							<p>
-								Checkout out this informative website about Poison Oak. You can
-								even play our game to see if you can identify it. It's not as
-								easy as it looks!
-							</p>
-							<p>Technology Used:</p>
-						</CardText>
-						<CardActions border style={{ padding: '0', alignItems: 'center' }}>
-							<Button colored>
-								<a
-									href="https://github.com/GarlandLai/PoisonOak"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									GitHub
-								</a>
-							</Button>
-
-							<Button colored>
-								<a
-									href="https://oak-fg.firebaseapp.com"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									Live Demo
-								</a>
-							</Button>
-							<CardMenu style={{ color: '#fff' }}>
-								<IconButton name="share" />
-							</CardMenu>
-						</CardActions>
-					</Card>
-				</div>
-			);
-		} else if (this.state.activeTab === 2) {
-			return (
-				// Project 3
-				<div className="project-grid">
-					<Card
-						shadow={5}
-						style={{
-							minWidth: '440px',
-							height: '450px',
-							margin: 'auto',
-						}}
-					>
-						<CardTitle
-							style={{
-								color: '#fff',
-								height: '225px',
-								background:
-									'url(https://create-react-app.dev/img/logo-og.png) center / cover',
-							}}
-						>
-							React Project #3
-						</CardTitle>
-						<CardText>Add description of project</CardText>
-						<CardActions border>
-							<Button colored>Github</Button>
-							<Button colored>Live Demo</Button>
-							<CardMenu style={{ color: '#fff' }}>
-								<IconButton name="share" />
-							</CardMenu>
-						</CardActions>
-					</Card>
-				</div>
-			);
-		}
-	}
-
 	render() {
 		return (
-			<div className="category-tabs">
-				<Tabs
-					activeTab={this.state.activeTab}
-					onChange={(tabId) => this.setState({ activeTab: tabId })}
-					ripple
-				>
-					<Tab>React</Tab>
-					<Tab>JavaScript</Tab>
-					<Tab>Ruby on Rails</Tab>
-				</Tabs>
+			<div className="project-container">
+				<h2>My Projects</h2>
+				<hr />
 				<Grid>
 					<Cell col={12}>
-						<div className="content">{this.toggleCategories()}</div>
+						<div className="project-grid">
+							<Card
+								shadow={5}
+								style={{
+									minWidth: '440px',
+									height: '450px',
+									margin: 'auto',
+								}}
+							>
+								<CardTitle
+									style={{
+										color: '#fff',
+										height: '225px',
+										background:
+											'url(https://create-react-app.dev/img/logo-og.png) center / cover',
+									}}
+								>
+									The Good Burger
+								</CardTitle>
+								<CardText style={{ height: '75px' }}>
+									<p>
+										Login or create account and create the perfect burger for
+										delivery!
+									</p>
+								</CardText>
+								<CardText style={{ height: '35px' }}>
+									<p>
+										<strong>Technology Used: </strong> React, Styled Components,
+										Firebase
+									</p>
+								</CardText>
+								<CardActions border style={{ padding: '0' }}>
+									<Button colored>
+										<a
+											href="https://github.com/GarlandLai/Burger-Builder"
+											target="_blank"
+											rel="noopener noreferrer"
+										>
+											GitHub
+										</a>
+									</Button>
+									<Button colored>
+										<a
+											href="https://react-my-burger-a16da.web.app"
+											target="_blank"
+											rel="noopener noreferrer"
+										>
+											Live Demo
+										</a>
+									</Button>
+									<CardMenu style={{ color: '#fff' }}>
+										<IconButton name="share" />
+									</CardMenu>
+								</CardActions>
+							</Card>
+							<Card
+								shadow={5}
+								style={{
+									minWidth: '440px',
+									height: '450px',
+									margin: 'auto',
+								}}
+							>
+								<CardTitle
+									style={{
+										color: '#fff',
+										height: '225px',
+										background:
+											'url(https://create-react-app.dev/img/logo-og.png) center / cover',
+									}}
+								>
+									ZIAI
+								</CardTitle>
+								<CardText style={{ height: '75px' }}>
+									Meditation journaling application. Listen to the meditation
+									and write some journal entries outlining you experiences!
+									<p>
+										Login or create account and create the perfect burger for
+										delivery!
+									</p>
+								</CardText>
+								<CardText style={{ height: '35px' }}>
+									<p>
+										<strong>Technology Used: </strong> React, Ruby on Rails,
+										postgreSQL
+									</p>
+								</CardText>
+
+								<CardActions
+									border
+									style={{ padding: '0', alignItems: 'center' }}
+								>
+									<Button colored>
+										<a
+											href="https://github.com/GarlandLai/Capstone-2.0"
+											target="_blank"
+											rel="noopener noreferrer"
+										>
+											GitHub Front-end
+										</a>
+									</Button>
+									<Button colored>
+										<a
+											href="https://github.com/GarlandLai/capstone-backend"
+											target="_blank"
+											rel="noopener noreferrer"
+										>
+											GitHub Back-end
+										</a>
+									</Button>
+									<Button colored>
+										<a
+											href="https://react-my-burger-a16da.web.app"
+											target="_blank"
+											rel="noopener noreferrer"
+										>
+											Live Demo
+										</a>
+									</Button>
+									<CardMenu style={{ color: '#fff' }}>
+										<IconButton name="share" />
+									</CardMenu>
+								</CardActions>
+							</Card>
+							<Card
+								shadow={5}
+								style={{
+									minWidth: '440px',
+									height: '450px',
+									margin: 'auto',
+								}}
+							>
+								<CardTitle
+									style={{
+										color: '#fff',
+										height: '225px',
+										background:
+											'url(https://create-react-app.dev/img/logo-og.png) center / cover',
+									}}
+								>
+									Posion Oak Field Guild
+								</CardTitle>
+								<CardText style={{ height: '75px' }}>
+									<p>
+										Checkout out this informative website about Poison Oak. You
+										can even play our game to see if you can identify it. It's
+										not as easy as it looks!
+									</p>
+								</CardText>
+								<CardText style={{ height: '35px' }}>
+									<p>
+										<strong>Technology Used: </strong>
+										JavaScript, JQuery, HTML/CSS, Bootstrap
+									</p>
+								</CardText>
+								<CardActions
+									border
+									style={{ padding: '0', alignItems: 'center' }}
+								>
+									<Button colored>
+										<a
+											href="https://github.com/GarlandLai/PoisonOak"
+											target="_blank"
+											rel="noopener noreferrer"
+										>
+											GitHub
+										</a>
+									</Button>
+
+									<Button colored>
+										<a
+											href="https://oak-fg.firebaseapp.com"
+											target="_blank"
+											rel="noopener noreferrer"
+										>
+											Live Demo
+										</a>
+									</Button>
+									<CardMenu style={{ color: '#fff' }}>
+										<IconButton name="share" />
+									</CardMenu>
+								</CardActions>
+							</Card>
+						</div>
 					</Cell>
 				</Grid>
 			</div>
