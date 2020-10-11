2296e623798170418bb132b37df340f51d9a0516
Garland Lai <garlandlai24@gmail.com>
2020-10-11 12:28:52 -0700

    change about and contact to functional componennt. Add useState hook to manage state in contact.js

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Grid, Cell } from 'react-mdl';
 import Avatar from '../assets/avatar2.png';
 
-class About extends Component {
-	state = {
-		showPhone: false,
-	};
-	render() {
-		return (
-			<Grid className="about-body">
-				<Cell col={4}>
-					<img src={Avatar} alt="avatar" style={{ height: '500px' }} />
-					<h2>Garland Lai</h2>
-				</Cell>
-				<Cell col={8}>
-					<h2>About Me</h2>
-					<hr />
-				</Cell>
-			</Grid>
-		);
-	}
-}
+const about = () => {
+	return (
+		<Grid className="about-body">
+			<Cell col={4}>
+				<img src={Avatar} alt="avatar" style={{ height: '500px' }} />
+				<h2>Garland Lai</h2>
+			</Cell>
+			<Cell col={8}>
+				<h2>About Me</h2>
+				<hr />
+			</Cell>
+		</Grid>
+	);
+};
 
-export default About;
+export default about;
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,91 +1,87 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 import Avatar3 from '../assets/avatar3.JPG';
 
-class Contact extends Component {
-	state = {
-		showPhone: false,
-	};
-	render() {
-		return (
-			<div className="contact-body">
-				<Grid className="contact-grid">
-					<Cell col={6}>
-						<h2>Garland Lai</h2>
-						<img src={Avatar3} alt="avatar" style={{ height: '300px' }} />
-						<p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>
-							Excited and eager to connect. I'd love to discuss how I can be of
-							value to your team!
-						</p>
-					</Cell>
-					<Cell col={6}>
-						<h2>Contact Me</h2>
-						<hr />
-						<div className="contact-list">
-							<List>
-								<ListItem>
-									<ListItemContent
+const Contact = () => {
+	const [isShowing, setShow] = useState(false);
+	return (
+		<div className="contact-body">
+			<Grid className="contact-grid">
+				<Cell col={6}>
+					<h2>Garland Lai</h2>
+					<img src={Avatar3} alt="avatar" style={{ height: '300px' }} />
+					<p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>
+						Excited and eager to connect. I'd love to discuss how I can be of
+						value to your team!
+					</p>
+				</Cell>
+				<Cell col={6}>
+					<h2>Contact Me</h2>
+					<hr />
+					<div className="contact-list">
+						<List>
+							<ListItem>
+								<ListItemContent
+									style={{
+										fontSize: '25px',
+										fontFamily: 'Anton',
+										fontWeight: 300,
+									}}
+								>
+									<i className="fa fa-phone-square" aria-hidden="true" />
+									<button
+										onClick={() => setShow(true)}
 										style={{
 											fontSize: '25px',
 											fontFamily: 'Anton',
 											fontWeight: 300,
+											border: 'none',
+											background: 'none',
 										}}
 									>
-										<i className="fa fa-phone-square" aria-hidden="true" />
-										<button
-											onClick={() => this.setState({ showPhone: true })}
-											style={{
-												fontSize: '25px',
-												fontFamily: 'Anton',
-												fontWeight: 300,
-												border: 'none',
-												background: 'none',
-											}}
-										>
-											{this.state.showPhone ? '(415) 517-1671' : 'Show Number'}
-										</button>
-									</ListItemContent>
-								</ListItem>
+										{isShowing ? '(415) 517-1671' : 'Show Number'}
+									</button>
+								</ListItemContent>
+							</ListItem>
 
-								<ListItem>
-									<ListItemContent
-										style={{
-											fontSize: '25px',
-											fontFamily: 'Anton',
-											fontWeight: 300,
-										}}
+							<ListItem>
+								<ListItemContent
+									style={{
+										fontSize: '25px',
+										fontFamily: 'Anton',
+										fontWeight: 300,
+									}}
+								>
+									<i className="fab fa-linkedin" aria-hidden="true" />
+									<a
+										href="https://www.linkedin.com/in/garland-lai"
+										target="_blank"
+										rel="noopener noreferrer"
+										style={{ color: 'black', textDecoration: 'none' }}
 									>
-										<i className="fab fa-linkedin" aria-hidden="true" />
-										<a
-											href="https://www.linkedin.com/in/garland-lai"
-											target="_blank"
-											rel="noopener noreferrer"
-											style={{ color: 'black', textDecoration: 'none' }}
-										>
-											linkedin.com/in/garland-lai
-										</a>
-									</ListItemContent>
-								</ListItem>
+										linkedin.com/in/garland-lai
+									</a>
+								</ListItemContent>
+							</ListItem>
 
-								<ListItem>
-									<ListItemContent
-										style={{
-											fontSize: '25px',
-											fontFamily: 'Anton',
-											fontWeight: 300,
-										}}
-									>
-										<i className="fa fa-envelope" aria-hidden="true" />
-										Garlandlai7@gmail.com
-									</ListItemContent>
-								</ListItem>
-							</List>
-						</div>
-					</Cell>
-				</Grid>
-			</div>
-		);
-	}
-}
+							<ListItem>
+								<ListItemContent
+									style={{
+										fontSize: '25px',
+										fontFamily: 'Anton',
+										fontWeight: 300,
+									}}
+								>
+									<i className="fa fa-envelope" aria-hidden="true" />
+									Garlandlai7@gmail.com
+								</ListItemContent>
+							</ListItem>
+						</List>
+					</div>
+				</Cell>
+			</Grid>
+		</div>
+	);
+};
 
 export default Contact;
