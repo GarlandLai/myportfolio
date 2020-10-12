d2cb60777abbb354e51cf618409c1c3faa59b954
Garland Lai <garlandlai24@gmail.com>
2020-10-12 13:40:51 -0700

    add some career content to aboutme page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -12,6 +12,31 @@ const about = () => {
 			<Cell col={8}>
 				<h2>About Me</h2>
 				<hr />
+				<h4>Career Wise</h4>
+				<p>
+					Hi there! I'm Garland. I first interested in Tech while working at
+					Google on the Self-Driving Car project. I remember thinking how if
+					used responsibily, technology could create such a positive impact on
+					people's lives.
+				</p>
+				<p>
+					Prior to my decision to switch career paths into Tech, I worked as a
+					Project Manager at a Wholesale Suppy Chain company that provided
+					building materials for large multi-family construction projects.
+					Although I have decided to move forward from this role, I am grateful
+					for the experience.
+				</p>
+				<p>
+					I decided to make the leap and jumpstart my career as a Web Developer.
+					I enrolled in Epicodus Web Development bootcamp. After 1000+ hours of
+					hands-on developing, I was abke to...... This experience has lead me
+					to my internship with Cascade Energy as a Software Developer. I gained
+					valuable knowledge, hands-on experience, and a deeper understanding of
+					writing quality code in large establish codebase. I learned so much
+					from the mentorship of senior software developers and have ingrained
+					an eagerness and excitement further my growth within the tech
+					industry.
+				</p>
 			</Cell>
 		</Grid>
 	);
