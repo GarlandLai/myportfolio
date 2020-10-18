7615e4b4912cb201a60ba338b430b7c26aa7d120
Garland Lai <garlandlai24@gmail.com>
2020-10-17 18:18:41 -0700

    update text alignment and content for career details

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -13,23 +13,17 @@ const about = () => {
 				<h2>About Me</h2>
 				<hr />
 				<h4>Career Wise</h4>
-				<p>
+				<p style={{textAlign: "left"}}>
 					Hi there! I'm Garland. Prior to my decision to switch career paths
 					into Tech, I worked as a Project Manager at a Wholesale Supply Chain
-					company that provided building materials for large multi-family
-					construction projects. Although I have decided to move forward from
-					this role, I am grateful for the experience.
+					company managing over 10+ large multi-family constructions projects. What I enjoyed most about this experience was the relationships built with customers and team members. Although I have decided to move forward from this role, I am grateful for the experience.
 				</p>
-				<p>
-					I decided to make the leap and jumpstart my career as a Web Developer.
-					I enrolled in Epicodus Web Development bootcamp. After 1000+ hours of
-					hands-on developing, I was abke to...... This experience has lead me
-					to my internship with Cascade Energy as a Software Developer. I gained
-					valuable knowledge, hands-on experience, and a deeper understanding of
-					writing quality code in large establish codebase. I learned so much
-					from the mentorship of senior software developers and have ingrained
-					an eagerness and excitement further my growth within the tech
-					industry.
+				<p style={{textAlign: "left"}}>
+					After 2 years, I decided to make the leap and invest in my interest in web development. I enrolled in 6-month Web Development course called Epicodus. With over 1000 hours of hands-on learning and problem solving, I was able to delve into the web development fundamentals and most important, gained a thrist for continously learning. 
+					</p>
+					<p style={{textAlign: "left"}}>
+					This experience led to my internship as a Software Developer with Cascade Energy. At Cascade, I gained valuable knowledge, hands-on experience, and a deeper understanding of
+					writing quality code in large established codebase. The mentorship of senior software developers have ingrained an eagerness and excitement further my growth within the tech industry.
 				</p>
 				<br />
 				<h4>Outside of Career</h4>
