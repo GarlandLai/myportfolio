265f8a7700ada83bd9704d5285bd749866c61b3e
Garland Lai <garlandlai24@gmail.com>
2020-10-18 10:48:38 -0700

    only have career info for about me. Also add some empty columns

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -9,30 +9,22 @@ const about = () => {
 				<img src={Avatar} alt="avatar" style={{ height: '500px' }} />
 				<h2>Garland Lai</h2>
 			</Cell>
-			<Cell col={8}>
+			<Cell col={1}></Cell>
+			<Cell col={6}>
 				<h2>About Me</h2>
 				<hr />
-				<h4>Career Wise</h4>
-				<p style={{textAlign: "left"}}>
-					Hi there! I'm Garland. Prior to my decision to switch career paths
-					into Tech, I worked as a Project Manager at a Wholesale Supply Chain
-					company managing over 10+ large multi-family constructions projects. What I enjoyed most about this experience was the relationships built with customers and team members. Although I have decided to move forward from this role, I am grateful for the experience.
+				<p>
+					Hi there! I'm Garland. Prior to my decision to switch career paths into Tech, I worked as a Project Manager at a Wholesale Supply Chain company managing over 10+ large multi-family constructions projects. What I enjoyed most about my time with Bridgewell Resources was the relationships I was able to create with team members and customers. Although I have decided to move forward from this role, I am grateful for the experience.
 				</p>
-				<p style={{textAlign: "left"}}>
-					After 2 years, I decided to make the leap and invest in my interest in web development. I enrolled in 6-month Web Development course called Epicodus. With over 1000 hours of hands-on learning and problem solving, I was able to delve into the web development fundamentals and most important, gained a thrist for continously learning. 
+				<p>
+					After 2 years, I decided to make the leap and invest in my interest in web development. I enrolled in 6-month Web Development course called Epicodus. With over 1000 hours of hands-on learning and problem solving, I was able to delve into the web development fundamentals and most importantly, I gained a thrist for continously learning. 
 					</p>
-					<p style={{textAlign: "left"}}>
-					This experience led to my internship as a Software Developer with Cascade Energy. At Cascade, I gained valuable knowledge, hands-on experience, and a deeper understanding of
-					writing quality code in large established codebase. The mentorship of senior software developers have ingrained an eagerness and excitement further my growth within the tech industry.
+					<p>
+					This experience led to my internship as a Software Developer with Cascade Energy. At Cascade, I gained valuable knowledge, hands-on experience, and a deeper understanding of writing quality code in large established codebase. The mentorship of senior software developers have ingrained an eagerness and excitement further my growth within the tech industry.
 				</p>
 				<br />
-				<h4>Outside of Career</h4>
-				<p>
-					During my free time, I enjoy being playing sports, learning new
-					things, reading, relaxing at the park/beach, and hanging out with
-					friends and family.
-				</p>
 			</Cell>
+			<Cell col={1}></Cell>
 		</Grid>
 	);
 };
