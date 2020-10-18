b00b9bda8949afa42ee22f50e2b2b5c9d9e354dd
Garland Lai <garlandlai24@gmail.com>
2020-10-18 10:53:30 -0700

    styling adjustments to about me

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -14,10 +14,10 @@ const about = () => {
 				<h2>About Me</h2>
 				<hr />
 				<p>
-					Hi there! I'm Garland. Prior to my decision to switch career paths into Tech, I worked as a Project Manager at a Wholesale Supply Chain company managing over 10+ large multi-family constructions projects. What I enjoyed most about my time with Bridgewell Resources was the relationships I was able to create with team members and customers. Although I have decided to move forward from this role, I am grateful for the experience.
+					Prior to my decision to switch career paths into Tech, I worked as a Project Manager at a Wholesale Supply Chain company managing over 10+ large multi-family constructions projects. What I enjoyed most about my time with Bridgewell Resources was the relationships I was able to create with team members and customers. Although I have decided to move forward from this role, I am grateful for the experience.
 				</p>
 				<p>
-					After 2 years, I decided to make the leap and invest in my interest in web development. I enrolled in 6-month Web Development course called Epicodus. With over 1000 hours of hands-on learning and problem solving, I was able to delve into the web development fundamentals and most importantly, I gained a thrist for continously learning. 
+					After 2 years, I decided to make the leap and invest in my interest in web development. I enrolled in 6-month Web Development course called Epicodus. With over 1000 hours of hands-on learning and problem solving, I was able to delve into the web development fundamentals and most importantly, gained a thrist for continously learning. 
 					</p>
 					<p>
 					This experience led to my internship as a Software Developer with Cascade Energy. At Cascade, I gained valuable knowledge, hands-on experience, and a deeper understanding of writing quality code in large established codebase. The mentorship of senior software developers have ingrained an eagerness and excitement further my growth within the tech industry.
